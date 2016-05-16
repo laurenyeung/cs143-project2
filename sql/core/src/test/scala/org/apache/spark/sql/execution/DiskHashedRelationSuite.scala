d4e1c7a7509e8b9250bcd191e8d206fab902150f
Michael Wang <micwa6@yahoo.com>
2016-05-16 15:01:47 -0700

    Tests for task 2

--- sql/core/src/test/scala/org/apache/spark/sql/execution/DiskHashedRelationSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/execution/DiskHashedRelationSuite.scala
@@ -35,4 +35,30 @@ class DiskHashedRelationSuite extends FunSuite {
 
     hashedRelation.closeAllPartitions()
   }
+
+  test("values are in correct partition AND are correct") {
+    val data: Array[Row] = (0 to 100).map(i => Row(i)).toArray
+    val hashedRelation: DiskHashedRelation = DiskHashedRelation(data.iterator, keyGenerator, 3, 64000)
+    var count: Int = 0
+
+    for (partition <- hashedRelation.getIterator()) {
+      for (row <- partition.getData()) {
+        assert(row.hashCode() % 3 == count)
+        assert(data.contains(row))
+      }
+      count += 1
+    }
+  }
+
+  test ("empty input with null checks") {
+    val data: ArraySeq[Row] = new ArraySeq[Row](0)
+    val hashedRelation: DiskHashedRelation = DiskHashedRelation(data.iterator, keyGenerator)
+
+    for (partition <- hashedRelation.getIterator()) {
+      val it = partition.getData()
+      assert(!it.hasNext && it.next() == null)
+    }
+
+    hashedRelation.closeAllPartitions()
+  }
 }
